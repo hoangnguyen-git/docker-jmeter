ad916682e02d1b8be4fa1ac2af4498f48c451894
hoangnguyenv <hoanv@netcompany.com>
2020-04-09 13:22:05 +0700

    add jmeter file

--- test.sh
+++ test.sh
@@ -18,11 +18,13 @@ rm -rf ${R_DIR} > /dev/null 2>&1
 mkdir -p ${R_DIR}
 
 /bin/rm -f ${T_DIR}/test-plan.jtl ${T_DIR}/jmeter.log  > /dev/null 2>&1
+/bin/rm -f ${T_DIR}/ApiGateway.Routing.Tests.jtl ${T_DIR}/ApiGateway.Routing.Tests.log  > /dev/null 2>&1
+/bin/rm -f ${T_DIR}/CoreAPI.Websockets.Test.jtl ${T_DIR}/CoreAPI.Websockets.Test.log  > /dev/null 2>&1
 
 ./run.sh -Dlog_level.jmeter=DEBUG \
 	-JTARGET_HOST=${TARGET_HOST} -JTARGET_PORT=${TARGET_PORT} \
 	-JTARGET_PATH=${TARGET_PATH} -JTARGET_KEYWORD=${TARGET_KEYWORD} \
-	-n -t ${T_DIR}/test-plan.jmx -l ${T_DIR}/test-plan.jtl -j ${T_DIR}/jmeter.log \
+	-n -t ${T_DIR}/ApiGateway.Routing.Tests.jmx -l ${T_DIR}/ApiGateway.Routing.Tests.jtl -j ${T_DIR}/jmeter.log \
 	-e -o ${R_DIR}
 
 echo "==== jmeter.log ===="
